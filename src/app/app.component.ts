84a7dab04ceea45b41902df555b1fa94c54d665c
abracu <abracu@novation.com.co>
2018-02-11 00:48:50 -0500

    Fin de la Aplicion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { resolve, reject } from 'q';
+import { setTimeout } from 'timers';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nombre = "Alfredo";
 
+  nombre2 = "alfredo bravo cuero";
+
   arreglo = [1,2,3,4,5,6,7,8,9,10];
 
   PI = Math.PI;
 
   a = 0.345;
+
+  salario = 2500000;
+
+  heroe = {
+    nombre: "Alfredo",
+    apellidos: "Bravo Cuero",
+    edad: 33,
+    direccion:{
+      ciudad: "Medellin",
+      barrion: "Navarra"
+    }
+  };
+
+  valorDePromesa = new Promise( (resolve, reject)=>{
+    setTimeout( ()=>resolve('Llego la data!!'),3500 );
+  });
+
+  fecha = new Date();
+
+  video = 'K1A0ua1Xhok';
 }
